5ffe4aac2420e5aadf05bd78376a4afdeb20b0d5
Pai Zhang <pai.zhang@oracle.com>
2020-03-22 12:36:08 -0700

    Modified EchoServerHandler

--- src/main/java/firstexample/EchoServerHandler.java
+++ src/main/java/firstexample/EchoServerHandler.java
@@ -1,5 +1,19 @@
 package firstexample;
 
-public class EchoServerHandler {
 
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        // ByteBuf is a random and sequential access of one or more bytes.
+        // Java doc: https://netty.io/4.0/api/io/netty/buffer/ByteBuf.html
+        ByteBuf in = (ByteBuf) msg;
+        System.out.println("System received: " + in.toString(CharsetUtil.UTF_8));
+
+    }
 }
